868c00b5305ec82860f0052ada80af3ff7929798
LuisL <luislunandroid@gmail.com>
2022-08-18 22:15:28 +0000

    version 7.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-
+import  { App } from "../js/index.js"
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
@@ -10,4 +9,4 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<div>Hola Mundo$$$</div>, document.querySelector("#app"));
